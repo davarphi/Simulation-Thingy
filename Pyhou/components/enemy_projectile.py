2d0071da8abef34b5c64f82199907eb6946b2c22
Ilman <dzubyan04@gmail.com>
2025-11-11 00:23:38 +0700

    qol : bullet is now white with colored lines like game

--- Pyhou/components/enemy_projectile.py
+++ Pyhou/components/enemy_projectile.py
@@ -23,8 +23,8 @@ class EnemyProjectile():
         self.pos += self.vel
     
     def draw(self, window):
-        pygame.draw.circle(window, (0, 0, 128, 50), (self.pos.x, self.pos.y), self.r)
-        pygame.draw.circle(window, (0, 0, 0, 50), (self.pos.x, self.pos.y), self.r, 1)
+        pygame.draw.circle(window, (255, 255, 255, 50), (self.pos.x, self.pos.y), self.r)
+        pygame.draw.circle(window, (0, 0, 128, 50), (self.pos.x, self.pos.y), self.r, 2)
 
     def is_out_bound(self):
         return (self.pos.y < -self.r/2 or 
